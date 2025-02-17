1eb8305f0a5feb2e7d7e25e7df7d454ceefd17ad
Hellom38 <98119185+hellom38@users.noreply.github.com>
2025-02-17 21:59:49 +0100

    Create ModameMenu.js

---
+++ hacks/ModameMenu.js
@@ -0,0 +1,461 @@
+var n = ["JORGE NO TOQUES NADA!!","NO CAMBIES EL HELLOM38 O VOY A HACKEAR TU CUENTA","Hellom38"];
+
+
+var trollstart = function () {
+alert("ERES TONTO");
+  document.write("ERES TONTO");
+  alert("cookie hecha llamada \"EL QUE LE ESTO ES TONTO\"");
+  location.redirect("https://www.youtube.com/watch?v=qUq50VAx6CU");
+};
+var GlobalHacks = function() {
+var z = prompt("[1] Quizlet live\n[2] blooket\n[3] Global\n[4] Exit");
+  if (z === "1") {
+  var u = document.createElement('script');
+  u.type='text/javascript';
+  document.body.appendChild(u);
+  u.src='https://raw.githubusercontent.com/glixzzy/quizlet-hack/main/dist/bundle.js';
+  }
+  if (z === "2") {
+  var BLOOKETVERSION='2.0';
+  var tn = document.createElement('script');
+  tn.type='text/javascript';
+  document.body.appendChild(tn);
+  tn.src='https://raw.githubusercontent.com/glixzzy/blooket-hack/main/global/spamOpenBoxes.js';
+  }
+  if (z === "3") {
+  location.replace("https://onlinegdb.com/b1bejQQbmP");
+  }
+  if (z === "4") {
+  start2();
+  }
+};
+var Games = function() {
+ var ln = prompt("[1] modame game\n[2] ball shooter\n[3] minecraft\n[4] Modame game beta\n[5] Admin abuse\n[6] exit");
+ if (ln === "1") {
+  location.replace("https://onlinegdb.com/Gt9M_oKsW");
+  }
+ if (ln === "2") {
+  var KICKASSVERSION='2.0';
+  var s = document.createElement('script');
+  s.type='text/javascript';
+  document.body.appendChild(s);
+  s.src='//hi.kickassapp.com/kickass.js';
+  void(0);
+ }
+ if (ln === "3") {
+  location.replace("https://trinket.io/embed/python/086de50cfb");
+ }
+ if (ln === "4") {
+  location.replace("https://onlinegdb.com/CBTc5TlJJ");
+ }
+ if (ln === "5") {
+  location.replace("https://onlinegdb.com/x87l9Xghs");
+ }
+ if (ln === "6") {
+  start2();
+ }
+};
+var WebChange = function() {
+var WebChange_menu = prompt("[1] create/change cookie\n[2] quizlet hack\n[3] Unblock websites\n[4] mine BTC\n[5] Clear history\n[6] source code\n[7] URI decoder\n[8] AutoClicker(Blooket)\n[9] break document\n[10] firefox\n[11] exit");
+if (WebChange_menu === "5") {
+  var Start = function(mili) {var l = Math.floor(Math.random() * 99999999); window.open("https://areaprivada.st-patricks.com/alumnos/notas_alumno.asp?ID={0195E867-314A-47A0-9EAD-4DB8238818E3}&codsistema=303&codestructura=G2021COLSEC11A&codestructuraprincipal=S2021COL&codconvocatoria=2&descripcionconvocatoria=1%AA%20Evaluaci%F3n&codalumno=" + l); setTimeout(() => {Start();}, mili);};
+  var mili = prompt("milisegundos");
+  Start(mili);
+  }
+  if (WebChange_menu === "6") {
+  document.write(```var n = ["JORGE NO TOQUES NADA!!","NO CAMBIES EL HELLOM38 O VOY A HACKEAR TU CUENTA","Hellom38"];
+
+
+var trollstart = function () {
+alert("ERES TONTO");
+  document.write("ERES TONTO");
+  alert("cookie hecha llamada \"EL QUE LE ESTO ES TONTO\"");
+  location.redirect("https://www.youtube.com/watch?v=qUq50VAx6CU");
+};
+var start4 = function() {
+var z = prompt("[1] Quizlet live\n[2] blooket\n[3] Global\n[4] Exit");
+  if (z === "1") {
+  var u = document.createElement('script');
+  u.type='text/javascript';
+  document.body.appendChild(u);
+  u.src='https://raw.githubusercontent.com/glixzzy/quizlet-hack/main/dist/bundle.js';
+  }
+  if (z === "2") {
+  var BLOOKETVERSION='2.0';
+  var tn = document.createElement('script');
+  tn.type='text/javascript';
+  document.body.appendChild(tn);
+  tn.src='https://raw.githubusercontent.com/glixzzy/blooket-hack/main/global/spamOpenBoxes.js';
+  }
+  if (z === "3") {
+  location.replace("https://onlinegdb.com/b1bejQQbmP");
+  }
+  if (z === "4") {
+  start2();
+  }
+};
+var start3 = function() {
+ var ln = prompt("[1] modame game\n[2] ball shooter\n[3] minecraft\n[4] Modame game beta\n[5] Admin abuse\n[6] exit");
+ if (ln === "1") {
+  location.replace("https://onlinegdb.com/Gt9M_oKsW");
+  }
+ if (ln === "2") {
+  var KICKASSVERSION='2.0';
+  var s = document.createElement('script');
+  s.type='text/javascript';
+  document.body.appendChild(s);
+  s.src='//hi.kickassapp.com/kickass.js';
+  void(0);
+ }
+ if (ln === "3") {
+  location.replace("https://trinket.io/embed/python/086de50cfb");
+ }
+ if (ln === "4") {
+  location.replace("https://onlinegdb.com/CBTc5TlJJ");
+ }
+ if (ln === "5") {
+  location.replace("https://onlinegdb.com/x87l9Xghs");
+ }
+ if (ln === "6") {
+  start2();
+ }
+};
+var WebChange = function() {
+var WebChange_menu = prompt("[1] create/change cookie\n[2] quizlet hack\n[3] Unblock websites\n[4] mine BTC\n[5] Clear history\n[6] source code\n[7] URI decoder\n[8] exit");
+if (y === "5") {
+  location.replace("https://docs.google.com/");
+  location.replace("https://classroom.google.com/u/0/h");
+  location.replace("https://docs.google.com/");
+  location.replace("https://classroom.google.com/u/0/h");
+  location.replace("https://docs.google.com/");
+  location.replace("https://classroom.google.com/u/0/h");
+  location.replace("https://classroom.google.com/u/0/h");
+  }
+  if (y === "6") {
+  document.write();
+  }
+  if (y === "3") {
+  var website = prompt("Full website:");
+  var key = "92a0f97c79904557f17477c7fce22933_sr98766_ooPq87";
+  location.replace("http://api.proxiesapi.com/?auth_key=" + key + "&url=" + website);
+  }
+  if (y === "2") {
+  alert("paste: [0].click();setTimeout(function(){for(var F = setTimeout(\";\"), i = 0; i < F; i++) clearTimeout(i)}, 5100); //Change 5100 to milliseconds");
+    WebChange();
+  }
+  if (y === "1") {
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+   let c = ca[i];
+   while (c.charAt(0) == ' ') {
+     c = c.substring(1);
+   }
+   if (c.indexOf(name) == 0) {
+     return c.substring(name.length, c.length);
+   }
+  }
+  return "";
+}
+
+function checkCookie() {
+ 
+    let user = getCookie("username");
+  if (user != "") {
+   alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your cookie name","");
+       var value = prompt("VALUE:");
+    if (user != "" && user != null) {
+      setCookie(user, value, 30);
+    }
+  }
+}
+checkCookie();
+  }
+  if (y === "4") {
+  alert("under dev");
+    WebChange();
+  }
+  if (y === "7") {
+  var x = prompt("TEXT");
+  var DECODED = decodeURIComponent(x);
+  alert(DECODED);
+  WebChange();
+  }
+  if (y === "8") {
+  start2();
+  }
+};
+var start2 = function() {
+var y = prompt("[1] feedback\n[2] Games\n[3] Hacks\n[4] random joke\n[5] Vine\n[6] Clear history\n[7] source code\n[8] Unblock website\n[9] create cookie\n[10] mine BTC\n[11] exit");
+  if (y === "1") {
+  prompt("Feedback");
+  start2();
+  }
+  if (y === "2") {
+  start3();
+  }  
+  if (y === "3") {
+  start4();
+  }
+  if (y === "4") {
+  var jokes = ["Tu vida","aguacate es 50% agua y el otro 50% porciento cate","Un personaje de minecraft le dice a otro Estas un poco cuadrado, le dice el otro, es que he ido al gimnasio","Sabeis porque una escoba esta contenta? porque esta ba-rriendo","una roca a otra: la vida es dura","Esa fotocopia es tuya o solo es impresion mia","Porque se suicido el libro de matematicas? porque tenia muchos problema","Que hace una abeja en un gimnasio? zumba!","Un raton le dice a otro, que haces ahí, y dice él, estoy relajandome un ratito","Un chiste le pega a otro, que chiste más malo","tu cara"];  
+  var l = Math.floor(Math.random() * 11);
+  alert(jokes[l]);
+  start2();
+  }  
+  if (y === "5") {
+  var vines = ["https://www.youtube.com/watch?v=qUq50VAx6CU","https://www.youtube.com/watch?v=W6oQUDFV2C0","https://www.youtube.com/watch?v=Obgnr9pc820","https://www.youtube.com/watch?v=FO3tpxxdzko","https://www.youtube.com/watch?v=uZEpN_F4IDI","https://www.youtube.com/watch?v=nqwjC2NA5Dc","https://www.youtube.com/watch?v=oVH3j31pV7Y","https://www.youtube.com/watch?v=eTMb2UkW4xY","https://www.youtube.com/watch?v=G7RgN9ijwE4"];    
+  f = Math.floor(Math.random() * 8);    
+  location.replace(vines[f]);  
+  }  
+ 
+  if (y === "12") {
+ 
+  }
+  else {
+  start2();
+  }
+  };
+var start = function() {
+  var x = prompt("Password:");
+  if (Encryption(x) === 1951553632) {
+  alert("Started");
+  start2();
+  }
+  else if (Encryption(x) === -275450668) {
+  trollstart();
+  }
+  else {  
+  alert("Incorrect");
+  alert("If you have any problems please contact with hellom38 or dev.hellom38.hireable@gmail.com");    
+  start();
+  }
+  };
+var Encryption = function(string) {
+   var hash = 0;  
+  if (!string) return hash;
+                 
+   for (i = 0; i < string.length; i++) {
+      char = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash;
+    }
+                 
+  return hash;
+};
+
+
+if (n[2] === "Hellom38") {
+alert("Script executor by: " + n[2]);
+start();
+}
+else{
+alert(n[2]);
+alert("NO DEBERIAS HABER TOCADO NADA");
+}```);
+  }
+  if (WebChange_menu === "3") {
+  var website = prompt("Full website:");
+  var key = "92a0f97c79904557f17477c7fce22933_sr98766_ooPq87";
+  location.replace("http://api.proxiesapi.com/?auth_key=" + key + "&url=" + website);
+  }
+  if (WebChange_menu === "2") {
+  alert("paste: [0].click();setTimeout(function(){for(var F = setTimeout(\";\"), i = 0; i < F; i++) clearTimeout(i)}, 5100); //Change 5100 to milliseconds");
+    WebChange();
+  }
+  if (WebChange_menu === "1") {
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+   let c = ca[i];
+   while (c.charAt(0) == ' ') {
+     c = c.substring(1);
+   }
+   if (c.indexOf(name) == 0) {
+     return c.substring(name.length, c.length);
+   }
+  }
+  return "";
+}
+
+function checkCookie() {
+ 
+    let user = getCookie("username");
+  if (user != "") {
+   alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your cookie name","");
+       var value = prompt("VALUE:");
+    if (user != "" && user != null) {
+      setCookie(user, value, 30);
+    }
+  }
+}
+checkCookie();
+  }
+  if (WebChange_menu === "4") {
+  alert("under dev");
+    WebChange();
+  }
+  if (WebChange_menu === "7") {
+  var x = prompt("TEXT");
+  var DECODED = decodeURIComponent(x);
+  alert(DECODED);
+  WebChange();
+  }
+  if (WebChange_menu === "8") {
+ var BLOOKETVERSION='2.0';
+    var Ask = prompt("this might be dangerous, write: I  might die");
+    if (Ask === "I might die") {
+    var repeat = prompt("TIMES:(less than 50)");
+    var x = 0;
+  while (x < repeat) {fetch("https://api.blooket.com/api/ users/unlockblook", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "es-ES,es;q=0.9,en;q=0.8",
+    "content-type": "text/plain",
+    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium \";v=\"98\", \"Google Chrome\";v=\"98\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "x-blooket-build": "9d0e75e8-9f23-4454-8416-4440980ee 08f"
+  },
+  "referrerPolicy": "no-referrer",
+  "body": "JRkwagsnWBPK+f6NgF5+Q4LVOGz0iFITk2EGXZDK4oUOSq 3HvLvyzedzHBQkmqsUDOU4uBxJ8+YF3dH0dLw=",
+  "method": "PUT",
+  "mode": "cors",
+  "credentials": "include"
+});
+var x = x + 1;
+  }
+  }
+    }
+   
+  if (WebChange_menu === "9") {
+ 
+  }
+  if (WebChange_menu === "10") {
+  document.write('<iframe frameborder="0" width="100%" height="500px" src="https://replit.com/@hellom38/firefox?embed=true"></iframe>');
+  }
+  if (WebChange_menu === "11") {
+  start2();
+  }
+};
+var Other = function () {
+var menu = prompt("[1] feedback\n[2] random vines\n[3] random jokes\n[4] Credits and other\n[5] autoban blooket script\n[6] exit");
+  if (menu === "1") {
+  prompt("Feedback");
+  start2();
+  }
+  if (menu === "3") {
+  var jokes = ["Tu vida","aguacate es 50% agua y el otro 50% porciento cate","Un personaje de minecraft le dice a otro Estas un poco cuadrado, le dice el otro, es que he ido al gimnasio","Sabeis porque una escoba esta contenta? porque esta ba-rriendo","una roca a otra: la vida es dura","Esa fotocopia es tuya o solo es impresion mia","Porque se suicido el libro de matematicas? porque tenia muchos problema","Que hace una abeja en un gimnasio? zumba!","Un raton le dice a otro, que haces ahí, y dice él, estoy relajandome un ratito","Un chiste le pega a otro, que chiste más malo","tu cara"];  
+  var l = Math.floor(Math.random() * 11);
+  alert(jokes[l]);
+  start2();
+  }  
+  if (menu === "2") {
+  var vines = ["https://www.youtube.com/watch?v=qUq50VAx6CU","https://www.youtube.com/watch?v=W6oQUDFV2C0","https://www.youtube.com/watch?v=Obgnr9pc820","https://www.youtube.com/watch?v=FO3tpxxdzko","https://www.youtube.com/watch?v=uZEpN_F4IDI","https://www.youtube.com/watch?v=nqwjC2NA5Dc","https://www.youtube.com/watch?v=oVH3j31pV7Y","https://www.youtube.com/watch?v=eTMb2UkW4xY","https://www.youtube.com/watch?v=G7RgN9ijwE4"];    
+  f = Math.floor(Math.random() * 8);    
+  location.replace(vines[f]);  
+  }  
+  if (menu === "4") {
+    alert("Produced by Happy Studios. and endorsed by Kajuweb");
+    alert("(c) Copyright 2022 Happy Studios. and Kajuweb");
+    alert("Languages: Javascript--> Interaction\nHTML--> Web Language\nCSS--> Style\nPHP and Ruby--> Server-side\nPython-->anything\nSQL--> Database");
+    alert("Companies: kajuweb, Happy Studios.\n Main programmer: Hellom38\nMain designer: N/A\n Beta tester: ravio and jaime");
+    alert("emails: dev.hellom38.hireable@gmail.com, jorge.echeverria@students.st-patricks.com and jaimelo09@gmail.com");
+    alert("Javascript basic: basic commands, if then, for n =?, and while");
+  alert("if you know these basic commands please tell me!!");
+   
+alert("V2.2.08 updates: clear bowser history!!!, bug fixes");
+  alert("v2.3 coming up, UPDATES: OPEN incognito window");
+    Other();
+  }
+  if (menu === "6") {
+  start2();
+  }
+  if (menu === "7") {
+  alert('var repeat = 100; var x = 0; while (x < repeat) {fetch("https://api.blooket.com/api/ users/unlockblook", { "headers": { "accept": "application/json, text/plain, */*", "accept-language": "es-ES,es;q=0.9,en;q=0.8", "content-type": "text/plain", "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium \";v=\"98\", \"Google Chrome\";v=\"98\"", "sec-ch-ua-mobile": "?0", "sec-ch-ua-platform": "\"Windows\"", "sec-fetch-dest": "empty", "sec-fetch-mode": "cors", "sec-fetch-site": "same-site", "x-blooket-build": "9d0e75e8-9f23-4454-8416-4440980ee 08f" }, "referrerPolicy": "no-referrer", "body": "JRkwagsnWBPK+f6NgF5+Q4LVOGz0iFITk2EGXZDK4oUOSq 3HvLvyzedzHBQkmqsUDOU4uBxJ8+YF3dH0dLw=", "method": "PUT", "mode": "cors", "credentials": "include" }); var x = x + 1; }');  
+}
+};
+var start2 = function() {
+var y = prompt("[1] Games\n[2] hacks\n[3] Web manipulation\n[4] other\n[5] exit");
+  if (y === "1") {
+  Games();
+  }
+  if (y === "2") {
+  GlobalHacks();
+  }
+  if (y === "3") {
+  WebChange();
+  }
+  if (y === "4") {
+  Other();
+  }
+  if (y === "5") {
+ 
+  }
+  else {
+  start2();
+  }
+  };
+var start = function() {
+  var x = prompt("Password:");
+  if (Encryption(x) === 890814323) {
+  alert("Started");
+  start2();
+  }
+  else if (Encryption(x) === -275450668) {
+  trollstart();
+  }
+  else {  
+  alert("Incorrect");
+  alert("If you have any problems please contact with hellom38 or dev.hellom38.hireable@gmail.com");    
+  start();
+  }
+  };
+var Encryption = function(string) {
+   var hash = 0;  
+  if (!string) return hash;
+                 
+   for (i = 0; i < string.length; i++) {
+      char = string.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash;
+    }
+                 
+  return hash;
+};
+var x = n[2];
+if (n[2] === "Hellom38") {
+alert("Script executor by: " + x);
+  start();
+} else {
+  alert(n[2]);
+  alert("NO DEBERIAS HABER TOCADO NADA");
+
+}
